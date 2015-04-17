4c33f8e8483c20111cd43172948c8e1ccc7a9aa1
maurice <maurice@maurices-MacBook-Pro.local>
2015-04-17 18:38:21 -0500

    Pre-competition fixes

--- scoreboard/clear_score_entries.py
+++ scoreboard/clear_score_entries.py
@@ -6,12 +6,29 @@ django.setup()
 
 from robotics_scoreboard.models import Team, Score
 
-#Get queryset that will return all score entries
-scores = Score.objects.all()
+def main():
+	#Get queryset that will return all score entries
+	scores = Score.objects.all()
 
-for score in scores:
-	zeroOutScore(score)
+	print ("Clearing {} scores".format(scores.count()))
+		
+	for score in scores:
+		zeroOutScore(score)
 
+	teams = Team.objects.all()
+
+	print ("Clearing {} Team entries".format(teams.count()))
+
+	for team in teams:
+		clearTeamStatus(team)
+
+def clearTeamStatus(team):
+    team.in_final=False
+    team.disqualified=False
+    team.total_score=0
+    team.perfect_score_payout=False
+    team.save()
+    return team
 
 def zeroOutScore( score ):
 	score.search_path = 0
@@ -19,7 +36,12 @@ def zeroOutScore( score ):
 	score.critical_path = 0
 	score.critical_time = 0.00
 	score.easter_egg = 0
-	score.penalty = 0
+	score.critical_penalty = 0
+	score.search_penalty = 0
 	score.round_score = 0
+	score.btb_percentage = 0
 	score.save()
 	return score
+
+if __name__ == '__main__':
+	main()
